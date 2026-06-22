7312daf220421f761fc0055d7596db9dc144a42f
Joachim Metz <joachim.metz@gmail.com>
2026-06-22 11:53:20 +0200

    Worked on tests and changes for CI tests

--- include/libfmos/error.h
+++ include/libfmos/error.h
@@ -82,7 +82,7 @@ enum LIBFMOS_ARGUMENT_ERROR
 	 */
 	LIBFMOS_ARGUMENT_ERROR_UNSUPPORTED_VALUE	= 8,
 
-	/* The argument contains a value that conficts with another argument
+	/* The argument contains a value that conflicts with another argument
 	 */
 	LIBFMOS_ARGUMENT_ERROR_CONFLICTING_VALUE	= 9
 };
@@ -214,7 +214,7 @@ enum LIBFMOS_OUTPUT_ERROR
 {
 	LIBFMOS_OUTPUT_ERROR_GENERIC			= 0,
 
-	/* There is insuficient space to write the output
+	/* There is insufficient space to write the output
 	 */
 	LIBFMOS_OUTPUT_ERROR_INSUFFICIENT_SPACE		= 1
 };
